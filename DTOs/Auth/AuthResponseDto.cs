712ad334922197100a4f43a8646c4748b797ab74
VIKRAM <717822p361@kce.ac.in>
2026-04-18 11:50:38 +0530

    Added Dto fro auth files

--- DTOs/Auth/AuthResponseDto.cs
+++ DTOs/Auth/AuthResponseDto.cs
@@ -1,6 +1,10 @@
-﻿namespace RetailOrdering.API.DTOs.Auth
+﻿namespace RetailOrdering.API.DTOs.Auth;
+
+public class AuthResponseDto
 {
-    public class AuthResponseDto
-    {
-    }
+    public string Token { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
 }
+
--- DTOs/Auth/RegisterDto.cs
+++ DTOs/Auth/RegisterDto.cs
@@ -2,5 +2,11 @@
 {
     public class RegisterDto
     {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+
     }
 }
