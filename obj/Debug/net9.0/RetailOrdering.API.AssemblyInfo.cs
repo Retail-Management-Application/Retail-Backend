37590f04d99f2d5a7877fdb323847e1fdfb2db73
VIKRAM <717822p361@kce.ac.in>
2026-04-18 14:16:09 +0530

    new commit

--- obj/Debug/net9.0/RetailOrdering.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/RetailOrdering.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetailOrdering.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39066fc3fa99f6b4242638cea0a50c8fe9534afc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bc829a6ae3b36eb5df0c6cad9b6c56b053aeba0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetailOrdering.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RetailOrdering.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
